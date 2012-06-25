e10b49ba3b4e3e5a89e7f768b7726a11adcb1d6b
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2012-06-25 17:14:50 +0300

    Added an utility for parsing md with lowlite highlighting

---
+++ build-files/md.js
@@ -0,0 +1,44 @@
+var parsemd = require('github-flavored-markdown').parse
+var lowlite = require('lowlite')
+var highlight = lowlite.highlight
+
+function md (str) {
+	var r, i, p, s, m, n, l, b
+
+	r = []
+	i = p = 0
+	s = ''
+
+	while ((i = str.indexOf('```', p)) !== -1) {
+		b = str[i-1] === '\n' || str[i-1] === '\r' ? 'block ' : ''
+
+		s += str.substr(p, i - p)
+
+		n = str.indexOf('```', i + 3)
+		if (n === -1) {
+			p += str.substr(i)
+			break
+		}
+
+		m = str.substr(i + 3, n - i)
+
+		l = /^\w+/.exec(m)
+		l = l && l[0]
+		if (!lowlite.lexers[l]) l = ''
+
+		s += '<code class="' + b + l + '">$' + r.length + ';</code>'
+
+		r.push(highlight(m.substr(l.length).trim(), {
+			lexer: l,
+			aliases: lowlite.shorthands
+		}))
+
+		p = n + 3
+	}
+
+	s += str.substr(p)
+
+	return parsemd(s).replace(/\$(\d+);/g, function (block, number) {
+		return r[~~number]
+	})
+}
